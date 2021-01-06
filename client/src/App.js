67312b0d8f6c17849de74916f9266c8910928297
Rajat Beladiya <rajatbeladiya7@gmail.com>
2021-01-06 08:48:24 +0530

    frontend added

---
+++ client/src/App.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
+import NewTransfer from './NewTransfer';
+import TransferList from './TransferList';
+import { getWeb3, getWallet } from './utils';
+
+function App() {
+  const [web3, setWeb3] = useState(undefined);
+  const [accounts, setAccounts] = useState(undefined);
+  const [wallet, setWallet] = useState(undefined);
+  const [approvers, setApprovers] = useState([]);
+  const [quorum, setQuorum] = useState(undefined);
+  const [transfers, setTransfers] = useState([]);
+
+  useEffect(() => {
+    const init = async () => {
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const wallet = await getWallet(web3);
+      const approvers = await wallet.methods.getApprovers().call();
+      const quorum = await wallet.methods.quorum().call();
+      const transfers = await wallet.methods.getTransfers().call();
+      setWeb3(web3);
+      setAccounts(accounts);
+      setWallet(wallet);
+      setApprovers(approvers);
+      setQuorum(quorum);
+      setTransfers(transfers);
+    };
+    init();
+  });
+
+  const createTransfer = transfer => {
+    wallet.methods
+      .createTransfer(transfer.amount, transfer.to)
+      .send({ from: accounts[0] });
+  }
+
+  const approveTransfer = transferId => {
+    wallet.methods
+      .approveTransfer(transferId)
+      .send({ from: accounts[0] });
+  }
+
+  if (typeof web3 === 'undefined' || typeof accounts === 'undefined' || typeof wallet === 'undefined'
+  || approvers.length === 0 || typeof quorum === undefined) {
+    console.log('typeof web3===',  typeof web3);
+    console.log('typeof accounts===',  typeof accounts);
+    console.log('typeof approvers.length===',  typeof approvers.length);
+    console.log('typeof quorum===',  typeof quorum);
+    return <div>Loading......</div>
+  }
+
+  return (
+    <div>
+      MultiSig Wallet
+      <Header approvers={approvers} quorum={quorum} />
+      <NewTransfer createTransfer={createTransfer} />
+      <TransferList transfers={transfers} approveTransfer={approveTransfer} />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/utils.js
@@ -0,0 +1,32 @@
+import Web3 from 'web3';
+import Wallet from './contracts/Wallet.json';
+
+export const getWeb3 = () => {
+  return new Promise((resolve, reject) => {
+    window.addEventListener('load', async () => {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          await window.ethereum.enable();
+          resolve(web3);
+        } catch (error) {
+          reject(error);
+        }
+      } else if (window.web3) {
+        resolve(window.web3);
+      } else {
+        reject('Must Install Metamask');
+      }
+    });
+  });
+  // return new Web3('http://localhost:9545');
+};
+
+export const getWallet = async web3 => {
+  const networkId = await web3.eth.net.getId();
+  const contractDeployment = Wallet.networks[networkId];
+  return new web3.eth.Contract(
+    Wallet.abi,
+    contractDeployment && contractDeployment.address
+  );
+}
